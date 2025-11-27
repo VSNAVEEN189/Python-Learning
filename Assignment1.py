64ed29f6468af5967e6ec236ce5ea4ceeefd7faf
V S NAVEEN KUMAR AMBATI <vsnaveenkumarambati2004@gmail.com>
2025-11-27 11:42:24 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,13 @@
+#Task 1: Perform Basic Mathematical Operations
+a = float(input("Enter a number: "))
+b = float(input("Enter another number: "))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#Task 2: Create a Personalized Greeting
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("Hello nice to meet you Mr." + full_name)
