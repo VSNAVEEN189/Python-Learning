ba57f4bbd67dfb18bc1b99732a61faf90ca9ae38
V s naveen kumar ambati <vsnaveenkumarambati2004@gmail.com>
2026-02-09 18:41:58 +0530

    Static

--- DJANGO/myproject1/myproject1/urls.py
+++ DJANGO/myproject1/myproject1/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("app1/", include("app1.urls"))  
+    path("app1/", include("app1.urls")),
+    path("", include("mywebsite.blogs.urls"))
 ]
 
