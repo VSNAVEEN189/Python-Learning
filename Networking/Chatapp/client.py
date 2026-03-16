11a7499f31ba316dbfde240a4bbda506e2ca1955
V s naveen kumar ambati <vsnaveenkumarambati2004@gmail.com>
2026-03-16 15:05:47 +0530

    Chat app UI

--- Networking/Chatapp/client.py
+++ Networking/Chatapp/client.py
@@ -1,4 +1,28 @@
 import socket
+from tkinter import *
+
+def send(listbox,entry):
+    message = entry.get()
+    listbox.insert('end',"Client :"+message)
+    entry.delete(0, END)
+    s.send(bytes(message,"utf-8"))   
+    recieve(listbox)
+
+def recieve(listbox):
+    message  = s.recv(50)
+    listbox.insert('end',"Server: "+message.decode('utf-8'))  
+
+root = Tk()
+
+entry = Entry()
+entry.pack(side=BOTTOM)
+listbox = Listbox(root)
+listbox.pack()
+button = Button(root,text="Send", command=lambda :send(listbox,entry))
+button.pack(side=BOTTOM)
+rbutton = Button(root,text="Recieve", command=lambda :recieve(listbox))
+rbutton.pack(side=BOTTOM)
+root.title('Client')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -7,10 +31,4 @@ PORT = 12345
 
 s.connect((HOST_NAME, PORT))
 
-while True:
-    message = s.recv(50)                     #Sending message from server to client
-    print("Server:"+message.decode('utf-8'))      
-
-    message_to_send = input('Client:')
-    s.send(bytes(message_to_send,"utf-8"))           
-    #Sending message from client to server
+root.mainloop()
--- Networking/Chatapp/server.py
+++ Networking/Chatapp/server.py
@@ -1,5 +1,29 @@
 import socket
 
+from tkinter import *
+
+def send(listbox,entry):
+    message = entry.get()
+    listbox.insert('end',"Server: "+message)
+    entry.delete(0,END)
+    client.send(bytes(message, "utf-8"))
+
+def recieve(listbox):
+    message_from_client = client.recv(50)        
+    listbox.insert('end',"Client: "+message_from_client.decode('utf-8'))    
+
+root = Tk()
+
+entry = Entry()
+entry.pack(side=BOTTOM)
+listbox = Listbox(root)
+listbox.pack()
+button = Button(root,text="Send", command=lambda :send(listbox,entry))
+button.pack(side=BOTTOM)
+rbutton = Button(root,text="Recieve", command=lambda :recieve(listbox))
+rbutton.pack(side=BOTTOM)
+root.title('Server')
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #For communication
 
 HOST_NAME = socket.gethostname()
@@ -9,11 +33,7 @@ s.bind((HOST_NAME, PORT))
 
 s.listen(4)
 client, address = s.accept()
-while True:
-    message = input('Server:')                        #Receiving message from server to client
-    client.send(bytes(message, "utf-8"))
-    message_from_client = client.recv(50)        
-    print("Client"+ message_from_client.decode('utf-8'))
-    #Receiving message from client to server
+
+root.mainloop()
 
 # This chat application is a single threaded application, This means action of receiving or sending messages from the client happens one at a time , Making sure server run first.
